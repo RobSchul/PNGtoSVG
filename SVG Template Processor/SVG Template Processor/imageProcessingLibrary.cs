fc0fa90ba3fcd5fe9845849995a9412724b1cab4
ril0n <robert.schultz@lander.edu>
2015-05-14 13:29:10 -0400

    update some

--- SVG Template Processor/SVG Template Processor/imageProcessingLibrary.cs
+++ SVG Template Processor/SVG Template Processor/imageProcessingLibrary.cs
@@ -6,59 +6,26 @@ using System.Threading.Tasks;
 using System.Drawing;
 using System.IO;
 using System.Drawing.Imaging;
+using System.Windows.Forms;
 
 
 namespace SVG_Template_Processor
 {
     class imageProcessingLibrary
     {
-        public static Rectangle[] fileChange(String file)
+        public Rectangle[] fileChange(String file)
         {
-            
+
             Bitmap myBitmap = new Bitmap(file);
-            Size ImageSize = myBitmap.Size;
             BitmapData bmData = myBitmap.LockBits(new Rectangle(0, 0, myBitmap.Width, myBitmap.Height), ImageLockMode.ReadOnly, myBitmap.PixelFormat);
+            List<Point> Points = findTPoints(myBitmap, bmData);
+             return mapTpoints(Points);
 
-            int pixelSize = 4;
-            int startX = 0;
-            int startY = 0;
-            int stopX = startX + myBitmap.Width;
-            int stopY = startY + myBitmap.Height;
-            int offset = bmData.Stride - myBitmap.Width * pixelSize;
-            List<Point> Points = new List<Point>();
+        }
+
+        private Rectangle[] mapTpoints(List<Point> Points)
+        {
             List<Rectangle> ret = new List<Rectangle>();
-            unsafe
-            {
-                // do the job
-                byte* ptr = (byte*)bmData.Scan0;
-
-                // allign pointer to the first pixel to process
-                ptr += (startY * bmData.Stride + startX);
-
-                for (int y = startY; y < stopY; y++)
-                {
-                    for (int x = startX; x < stopX; x++)
-                    {
-                        // process each pixel
-                        for (int p = 0; p < pixelSize; p++, ptr++)
-                        {
-                            switch (p)
-                            {
-                                case 3:
-                                    if (*ptr == 0)
-                                    {
-                                        Points.Add(new Point(x, y));
-                                    }
-                                    break;
-                            }
-                        }
-                    }
-                    ptr += offset;
-                }
-            }
-            myBitmap.UnlockBits(bmData);
-            myBitmap.Dispose();
-            myBitmap = null;
             while (Points.Count > 0)
             {
                 Point Base = Points[0];
@@ -66,46 +33,95 @@ namespace SVG_Template_Processor
                 List<Point> RecPoints = (from P in Points where P.X == Base.X || P.Y == Base.Y select P).ToList();
                 foreach (Point P in RecPoints)
                 {
-                    if (P.X == Base.X && P.Y == (BaseR.Y + BaseR.Height) + 1) BaseR.Height++;
-                    if (P.Y == Base.Y && P.X == (BaseR.X + BaseR.Width) + 1) BaseR.Width++;
+                    if (P.X == Base.X && P.Y == (BaseR.Y + BaseR.Height) + 1)
+                        BaseR.Height++;
+                    if (P.Y == Base.Y && P.X == (BaseR.X + BaseR.Width) + 1)
+                        BaseR.Width++;
                 }
                 Points.RemoveAll(P => BaseR.Contains(P));
-                if (BaseR.Width > 1 && BaseR.Height > 1) ret.Add(BaseR);
+                if (BaseR.Width > 1 && BaseR.Height > 1)
+                    ret.Add(BaseR);
             }
-
             return ret.ToArray();
-            
         }
-        
-        public static Bitmap Transparent2Color(Bitmap bmp1, Color target)
+
+        private unsafe List<Point> findTPoints(Bitmap myBitmap, BitmapData bmData)
         {
-            Bitmap bmp2 = new Bitmap(bmp1.Width, bmp1.Height);
-            Rectangle rect = new Rectangle(Point.Empty, bmp1.Size);
-            using (Graphics G = Graphics.FromImage(bmp2))
+            int pixelSize = 4, startX = 0, startY = 0;
+            int stopX = startX + myBitmap.Width;
+            int stopY = startY + myBitmap.Height;
+            int offset = bmData.Stride - myBitmap.Width * pixelSize;
+            List<Point> Points = new List<Point>();
+
+
+            byte* point = (byte*)bmData.Scan0;
+            //point to the first pixel 
+            point += (startY * bmData.Stride + startX);
+
+            for (int y = startY; y < stopY; y++)
             {
-                G.Clear(target);
-                
-                G.DrawImageUnscaledAndClipped(bmp1, rect);
-            }
-            return bmp2;
-        }
-
-         static void Main()
-        {
-            
-                try
-                {   Rectangle[] rNew = new Rectangle[10];
-                rNew = fileChange(@"\\chptfs\Shared\Intern Projects\SVG Template Creation\pngTemplatesApetureAreas\1141_2226x1047_11ozMug_4up_Misc_LoveNeverFailsLabel.png");
-                }
-            catch(Exception e)
+                for (int x = startX; x < stopX; x++)
                 {
+                    for (int p = 0; p < pixelSize; p++, point++)
+                        switch (p)
+                        {
+                            case 3:
+                                if (*point == 0)
+                                    Points.Add(new Point(x, y));
 
-            }
-            finally{
-                    
+                                break;
+                        }
                 }
+                point += offset;
+            }
+            myBitmap.UnlockBits(bmData);
+            myBitmap.Dispose();
+            myBitmap = null;
+            return Points;
 
         }
+
+
+        public Bitmap Transparent2Color(String file)
+        {
+            Image image = Image.FromFile(file);
+            Bitmap myBitmap = new Bitmap(file);
+            Color white = Color.Blue;
+            Bitmap bmp2 = new Bitmap(myBitmap.Width, myBitmap.Height);
+            Rectangle[] rect = fileChange(file);
+
+            SolidBrush blueBrush = new SolidBrush(Color.Blue);
+
+            for (int i = 0; i < rect.Length; i++)
+            {
+                Rectangle r;
+                r = rect[i];
+                System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(Color.Red);
+                Graphics g = Graphics.FromImage(myBitmap);
+                g.FillRectangle(myBrush, r);
+                myBrush.Dispose();
+                string text2 = "" + i;
+                Font font2 = new Font("Arial", 40, FontStyle.Bold, GraphicsUnit.Point);
+
+
+                TextFormatFlags flags = TextFormatFlags.WordBreak;
+                TextRenderer.DrawText(g, text2, font2, r, Color.Blue, flags);
+                g.Dispose();
+            }
+
+            /*for (int x = 0; x < myBitmap.Width; x++)
+                for (int y = 0; y < myBitmap.Height; y++)
+                {
+                    c = myBitmap.GetPixel(x, y);
+                    myBitmap.SetPixel(x, y, ((((short)(c.A)) & 0x00FF) <= 0) ? white : c); //replace 0 here with some higher tolerance if needed
+                }
+        */
+            return myBitmap;
+
+        }
+
+
+
 
     }
 }
